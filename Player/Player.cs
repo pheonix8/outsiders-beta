6f9aa27294454f9051b29144e93bbbce90a3e4d4
Leo Oetterli <61965711+pheonix8@users.noreply.github.com>
2024-09-14 00:38:57 +0200

    updated Player.cs & Player.tscn added animations

--- Player/Player.cs
+++ Player/Player.cs
@@ -3,11 +3,36 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+    public Vector2 ScreenSize;
+    public AnimatedSprite2D Sprite;
     [Export] public int Speed { get; set; } = 200;
-
+    
+    public override void _Ready()
+    {
+        ScreenSize = GetViewportRect().Size;
+        Sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    }
+    
+    public override void _Process(double delta)
+    {
+        switch (Velocity.Y)
+        {
+            case < 0:
+                Sprite.Play("up");
+                break;
+            case > 0:
+                Sprite.Play("down");
+                break;
+            case 0:
+                Sprite.Play("straight");
+                break;
+        }
+    }
+    
     public override void _PhysicsProcess(double delta)
     {
         var velocity = Vector2.Zero;
+        
         if (Input.IsActionPressed("move_right"))
         {
             velocity.X += 1;
@@ -27,10 +52,14 @@ public partial class Player : CharacterBody2D
         {
             velocity.Y -= 1;
         }
+        
+        Velocity = velocity.Normalized() * Speed;
 		
-
-        velocity = velocity.Normalized() * Speed;
-		
-        Position += velocity * (float)delta;
+        Position += Velocity * (float)delta;
+        
+        Position = new Vector2(
+            x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+            y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+        );
     }
 }
